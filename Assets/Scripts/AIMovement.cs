cd87b08995d297b2bd2466ccb69fac9ac8d2e675
Russell Loh <e0310763@u.nus.edu>
2018-10-23 00:12:09 +0800

    Added animations, rigidbodies and forward ai movement
    -Added animations
    -Added rigidbody & colliders
    -Added basic ai movement script
    -Set creatures as prefabs

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour {
+
+    public float moveX = 0;
+    public float moveY = 0;
+    public float moveZ = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        Vector3 moveForward = new Vector3(moveX, moveY, moveZ);
+        rb.velocity = moveForward;
+	}
+}
