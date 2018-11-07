b56b1c8fa70377506ecfc14e858110588b5239dc
Russell Loh <e0310763@u.nus.edu>
2018-11-07 15:09:35 +0800

    Added start and end game actions, Minor amendment to UI, Removed buttons
    -Added start and end game actions
    -Minor amendment to UI
    -Removed buttons from UI as input is using click instead

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,10 +10,13 @@ public class GameController : MonoBehaviour {
     public GameObject[] dynamicSpawn;
     public GameObject[] staticSpawn;
     public float spawnInterval = 3.0f;
+    public int gameState = 0;
 
     public int score;
     public Text scoreText;
     public Text timerText;
+    public GameObject startPanel;
+    public GameObject endPanel;
 
     public AudioClip soundEffect;
     public AudioClip background;
@@ -23,11 +26,12 @@ public class GameController : MonoBehaviour {
     float timer;
 
     public int gameTime = 30;
+    int initialGameTime = 0;
 
 	// Use this for initialization
 	void Start () {
-        StartCoroutine("LoseTime");
         timer = spawnInterval;
+        initialGameTime = gameTime;
         soundSource.clip = soundEffect;
         bgSource.clip = background;
         bgSource.Play();
@@ -35,29 +39,51 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        timerText.text = "Time: " + gameTime;
-        if (gameTime <= 0) {
-            StopCoroutine("LoseTime");
-        }
-
-        if(timer <= 0){
-            GameObject monster;
-            Vector3 spawnPos;
-            if(Random.Range(0,2) == 1){
-                monster = staticMonsters[(int)Random.Range(0, staticMonsters.Length)];
-                spawnPos = staticSpawn[(int)Random.Range(0, staticSpawn.Length)].transform.position;
-            }
-            else{
-                monster = dynamicMonsters[(int)Random.Range(0, dynamicMonsters.Length)];
-                spawnPos = dynamicSpawn[(int)Random.Range(0, dynamicSpawn.Length)].transform.position;
+        if (gameState == 1)
+        {
+            timerText.text = "Time: " + gameTime;
+            if (gameTime <= 0)
+            {
+                stopSpawn();
+                StopCoroutine("LoseTime");
             }
 
-            Quaternion monsterRotation = Quaternion.Euler(monster.transform.rotation.x, Random.Range(0, 360), monster.transform.rotation.z);
-            Instantiate(monster, spawnPos, monsterRotation);
+            if (timer <= 0)
+            {
+                GameObject monster;
+                Vector3 spawnPos;
+                if (Random.Range(0, 2) == 1)
+                {
+                    monster = staticMonsters[(int)Random.Range(0, staticMonsters.Length)];
+                    spawnPos = staticSpawn[(int)Random.Range(0, staticSpawn.Length)].transform.position;
+                }
+                else
+                {
+                    monster = dynamicMonsters[(int)Random.Range(0, dynamicMonsters.Length)];
+                    spawnPos = dynamicSpawn[(int)Random.Range(0, dynamicSpawn.Length)].transform.position;
+                }
 
-            timer = spawnInterval;
+                Quaternion monsterRotation = Quaternion.Euler(monster.transform.rotation.x, Random.Range(0, 360), monster.transform.rotation.z);
+                Instantiate(monster, spawnPos, monsterRotation);
+
+                timer = spawnInterval;
+            }
+            timer -= Time.deltaTime;
         }
-        timer -= Time.deltaTime;
+        else if(gameState == 2){
+            //END GAME
+            if (Input.GetMouseButtonDown(0))
+            {
+                startSpawn();
+            }
+        }
+        else{
+            //START GAME
+            if (Input.GetMouseButtonDown(0))
+            {
+                startSpawn();
+            }
+        }
 	}
 
     public void AddScore(int newScoreValue) {
@@ -68,6 +94,22 @@ public class GameController : MonoBehaviour {
 
     void UpdateScore() {
         scoreText.text = "Score: " + score;
+    }
+
+    void startSpawn()
+    {
+        timer = spawnInterval;
+        gameTime = initialGameTime;
+        startPanel.SetActive(false);
+        endPanel.SetActive(false);
+        StartCoroutine("LoseTime");
+        gameState = 1;
+    }
+
+    void stopSpawn()
+    {
+        endPanel.SetActive(true);
+        gameState = 2;
     }
 
     IEnumerator LoseTime() {
